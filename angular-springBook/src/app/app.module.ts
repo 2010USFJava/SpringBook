6ff37d7fcd1a90695ddcc753891afebe9de3e197
osbe0012 <chris.osberg@gmail.com>
2020-12-19 12:02:03 -0500

    Created update-profile and reset-password components

--- angular-springBook/src/app/app.module.ts
+++ angular-springBook/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdateProfileComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule
