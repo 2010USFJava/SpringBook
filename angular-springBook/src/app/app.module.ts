949bb3ab1611b048b55272eeab68697258a0df73
arisolivia <aocarrol@gmail.com>
2020-12-20 11:28:01 -0500

    added search-bar and -results component

--- angular-springBook/src/app/app.module.ts
+++ angular-springBook/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchBarComponent,
+    SearchResultsComponent,
   ],
   imports: [
     BrowserModule
