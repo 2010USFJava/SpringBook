6d72033b511f4e70833c157b42394d79c3fc4da8
Jennifer Heermance <jennifer.heermance@gmail.com>
2020-12-20 11:01:48 -0600

    shell create-new-post component added

--- angular-springBook/src/app/app.module.ts
+++ angular-springBook/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateNewPostComponent } from './create-new-post/create-new-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateNewPostComponent
   ],
   imports: [
     BrowserModule
