0ddc1b0a52afe1bd34f6118f7328d823b1d66ad5
osbe0012 <chris.osberg@gmail.com>
2020-12-23 07:31:23 -0500

    Changed employee references to users references

--- angular-springBook/src/app/employee.ts
+++
@@ -1,7 +0,0 @@
-export class Employee {
-    firstName: string = "First Name";
-    lastName: string = "Last Name";
-    email: string = "Email";
-    username: string = "Username";
-    password: string = "Password";
-}
--- angular-springBook/src/app/update-profile/update-profile.component.ts
+++ angular-springBook/src/app/update-profile/update-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, Inject, Input, EventEmitter } from '@angular/core';
-import { Employee } from 'src/app/employee';
+import { Users } from 'src/app/users';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { DOCUMENT } from '@angular/common';
@@ -12,17 +12,17 @@ import { DOCUMENT } from '@angular/common';
 })
 export class UpdateProfileComponent implements OnInit {
 
-  @Input() employee: Employee;
-  @Output() employeeChange = new EventEmitter<Employee>();
+  @Input() user: Users;
+  @Output() employeeChange = new EventEmitter<Users>();
 
   constructor(private http: HttpClient, @Inject(DOCUMENT) private _document: Document) {
-    this.employee = new Employee();
+    this.user = new Users();
   }
 
   ngOnInit(): void {
   }
 
-  updateProfile(employee: Employee): void {
+  updateProfile(employee: Users): void {
     JSONemployee: JSON.stringify(employee);
     this.http.post<any>('/placeholder', {});
     this._document.defaultView.location.reload();
